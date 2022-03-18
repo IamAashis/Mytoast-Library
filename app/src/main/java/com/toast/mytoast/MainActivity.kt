e60eb9ff040b74ce29e4a7c29658300cd027bb08
Aashis Thapa <aashis.thapa@ekbana.info>
2022-03-18 16:25:03 +0545

    Add Application Module

--- app/src/main/java/com/toast/mytoast/MainActivity.kt
+++ app/src/main/java/com/toast/mytoast/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.toast.mytoast
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.squareup.picasso.Picasso
+import com.toast.mytoastlibrary.ToastMessage
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var imvPicassso: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imvPicassso = findViewById(R.id.imvPicassso)
+
+    /*    Glide.with(this)
+            .load("https://cdn2.iconfinder.com/data/icons/sports-i-color/300/25-512.png")
+            .centerCrop()
+            .into(imvPicassso)*/
+//        ToastMessage().myToast(this, "Hello")
+//        ToastMessage().loadProfile("https://cdn2.iconfinder.com/data/icons/sports-i-color/300/25-512.png", imvPicassso)
+
+       ToastMessage().customToast(this, imvPicassso, "https://i.imgur.com/DvpvklR.png")
+
+
+        ToastMessage().myToast(this, "This is Toast Message")
     }
+
+
 }
--- mytoastlibrary/src/main/java/com/toast/mytoastlibrary/ToastMessage.kt
+++ mytoastlibrary/src/main/java/com/toast/mytoastlibrary/ToastMessage.kt
@@ -1,13 +1,58 @@
 package com.toast.mytoastlibrary
 
 import android.content.Context
+import android.widget.ImageView
 import android.widget.Toast
+import com.squareup.picasso.Picasso
+import es.dmoral.toasty.Toasty
 
 
 /**
  * Created by Aashis on 3/10/2022.
  */
 class ToastMessage {
+
+    fun myToast(c: Context?, message: String?) {
+        Toasty.error(c!!, message!!, Toast.LENGTH_SHORT, true).show()
+    }
+
+    fun customToast(context: Context, imageView: ImageView, url: String) {
+/*
+        val customLayout = LinearLayout.inflate(context, R.layout.toast_custom, null)
+
+        val imageView = customLayout.findViewById<ImageView>(R.id.imvLayout)
+        val msgText = customLayout.findViewById<TextView>(R.id.text)
+
+        msgText.text = toastText
+*/
+
+        Picasso.get().load(url).into(imageView);
+/*
+        val toast = Toast(context)
+        toast.duration = Toast.LENGTH_SHORT
+        toast.setGravity(Gravity.BOTTOM, 0, 0)
+        toast.view = customLayout
+        toast.show()
+*/
+
+    }
+
+    /*   fun showToast() {
+           val inflater = layoutInflater
+           val layout: View = inflater.inflate(
+               android.R.layout.,
+               findViewById<View>(android.R.id.toast_root) as ViewGroup
+           )
+           val toastText: TextView = layout.findViewById(android.R.id.toast_text)
+           val toastImage: ImageView = layout.findViewById(android.R.id.toast_image)
+           toastText.text = "This is a different text"
+   //        toastImage.setImageResource(R.drawable.ic_toasticon)
+           val toast = Toast(applicationContext)
+           toast.setGravity(Gravity.CENTER, 0, 0)
+           toast.duration = Toast.LENGTH_LONG
+           toast.setView(layout)
+           toast.show()
+       }*/
 
     fun showShortToast(c: Context?, message: String?) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
