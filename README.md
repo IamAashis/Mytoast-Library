682b7138885f1e64d2b93b5d07e55f1d49d30584
Aashis <aashis.thapa@ekbana.info>
2022-07-25 18:05:45 +0545

    Update read.md file

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Mytoast-Library
+
 A Simple Library to show toast
+
+## Prerequisites
+
+Add this in your root `build.gradle` file
+
+```gradle
+allprojects {
+		repositories {
+			...
+			maven { url 'https://jitpack.io' }
+		}
+	}
+```
+
+## Dependency
+
+Add this to your module's `build.gradle` file (make sure the version matches the JitPack badge
+above):
+
+```gradle
+dependencies {
+       implementation 'com.github.IamAashis:Mytoast-Library:1.0.0'
+}
+```
--- mytoastlibrary/src/main/java/com/toast/mytoastlibrary/ToastMessage.kt
+++ mytoastlibrary/src/main/java/com/toast/mytoastlibrary/ToastMessage.kt
@@ -1,42 +1,22 @@
 package com.toast.mytoastlibrary
 
 import android.content.Context
+import android.graphics.drawable.Drawable
+import android.view.Gravity
+import android.view.View
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
 import com.squareup.picasso.Picasso
-import es.dmoral.toasty.Toasty
 
 
 /**
  * Created by Aashis on 3/10/2022.
  */
 class ToastMessage {
-
-    fun myToast(c: Context?, message: String?) {
-        Toasty.error(c!!, message!!, Toast.LENGTH_SHORT, true).show()
-    }
-
-    fun customToast(context: Context, imageView: ImageView, url: String) {
-/*
-        val customLayout = LinearLayout.inflate(context, R.layout.toast_custom, null)
-
-        val imageView = customLayout.findViewById<ImageView>(R.id.imvLayout)
-        val msgText = customLayout.findViewById<TextView>(R.id.text)
-
-        msgText.text = toastText
-*/
-
-        Picasso.get().load(url).into(imageView);
-/*
-        val toast = Toast(context)
-        toast.duration = Toast.LENGTH_SHORT
-        toast.setGravity(Gravity.BOTTOM, 0, 0)
-        toast.view = customLayout
-        toast.show()
-*/
-
-    }
-
+    private var imageView: ImageView? = null
+    private var msgText: TextView? = null
 
     fun showShortToast(c: Context?, message: String?) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
@@ -46,4 +26,46 @@ class ToastMessage {
         Toast.makeText(c, message, Toast.LENGTH_LONG).show()
     }
 
+    fun showToastWithIcon(
+        context: Context,
+        message: String?,
+        icon: Drawable? = null,
+        url: String? = null
+    ) {
+        customToast(context, icon, message, url)
+    }
+
+    private fun customToast(
+        context: Context,
+        imageViewDrawable: Drawable?,
+        msg: String?,
+        urlImageUrl: String?
+    ) {
+        val view = setLayout(context)
+        msgText?.text = msg
+
+        if (!urlImageUrl.isNullOrEmpty())
+            Picasso.get().load(urlImageUrl).into(imageView)
+        else
+            imageView?.setImageDrawable(imageViewDrawable)
+
+        val toast = Toast(context)
+        toast.duration = Toast.LENGTH_SHORT
+        toast.setGravity(Gravity.CENTER, 0, 0)
+        toast.view = view
+        toast.show()
+    }
+
+    private fun setLayout(context: Context): View? {
+        val customLayout = LinearLayout.inflate(context, R.layout.toast_custom, null)
+
+        imageView = customLayout.findViewById(R.id.imvToastImage)
+        msgText = customLayout.findViewById(R.id.txvToastTitle)
+
+        return customLayout.rootView
+    }
+
+    class toastSetup() {
+
+    }
 }
